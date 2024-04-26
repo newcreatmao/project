1cffa1defae15ea13c3b84354a86dd56ef4869ad
“mao” <1489735794@qq.com>
2024-04-26 15:31:16 +0800

    use

---
+++ qt/creat/untitled/main.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include<QSqlDatabase>
+#include <QApplication>
+#include <qdebug.h>
+#include <QMessageBox>
+#include <QSqlError>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("192.168.142.131");
+    db.setPort(3306);
+    db.setDatabaseName("y");
+    db.setUserName("root");
+    db.setPassword("123456");
+    bool ok = db.open();
+    if (ok){
+        qDebug()<<"SECCES";
+    }
+    else {
+        qDebug()<<"error open database because"<<db.lastError().text();
+    }
+
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
