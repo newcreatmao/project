eb0c9c24df0b56347fb316a196827acdf241cba0
“mao” <1489735794@qq.com>
2024-04-30 16:26:44 +0800

    use

--- sql/sql_c++/main.cpp
+++ sql/sql_c++/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main() {
 	Mysql use;
-	use.Connection("192.168.142.131", "root","123456", "y", 3306);
-	use.Show_Table_Data("emp");
-	use.Get_Table_struct("emp");
+	use.Connection("192.168.142.131", "root","123456", "JINRONG", 3306);
+	use.Show_Table_Data("fund");
+	use.Get_Table_struct("fund");
+	use.Show_Table_Data("client");
 	return 0;
 }
