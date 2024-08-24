71635a1f79671ae8e5bb613d68b024be37353c88
“mao” <1489735794@qq.com>
2024-08-24 20:50:39 +0800

    use

---
+++ cmake/build/CMakeFiles/use.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use.dir/main.cpp.obj"
+  "CMakeFiles/use.dir/main.cpp.obj.d"
+  "libuse.dll.a"
+  "use.exe"
+  "use.exe.manifest"
+  "use.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/use.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
